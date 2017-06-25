f9efe2e795a6f680fc30931a3a6dd139fdc933f5
yamachi4416 <yamachi4416@gmail.com>
2017-06-25 14:13:22 +0900

    Remove チャット画面 RoomControllerの不要なメソッド、ビュー

--- src/ChatApp/Features/Room/RoomController.cs
+++ src/ChatApp/Features/Room/RoomController.cs
@@ -22,65 +22,6 @@ namespace ChatApp.Features.Room
         {
             return View();
         }
-
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind(include: "Name,Description")]ChatRoom room)
-        {
-            if (ModelState.IsValid)
-            {
-                CreateModel(room);
-                CreateModel(new ChatRoomMember
-                {
-                    UserId = GetCurrentUserId(),
-                    ChatRoom = room,
-                    IsAdmin = true
-                });
-
-                await _db.SaveChangesAsync();
-
-                return RedirectToAction(nameof(Edit), new { id = room.Id });
-            }
-
-            return View(room);
-        }
-
-        public async Task<IActionResult> Edit(Guid? id)
-        {
-            var query = _db.ChatRooms.Where(r => r.Id == id);
-            var room = await query.SingleOrDefaultAsync();
-
-            if (room == null)
-            {
-                return NotFound();
-            }
-
-            return View(room);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit([Bind(include: "Id,Name,Description")]ChatRoom room)
-        {
-            if (ModelState.IsValid)
-            {
-                var chatRoom = await _db.ChatRooms
-                    .Where(r => r.Id == room.Id)
-                    .SingleOrDefaultAsync();
-
-                UpdateModel(from: room, to: chatRoom, keys: "Id,Name,Description");
-
-                await _db.SaveChangesAsync();
-
-                room = chatRoom;
-            }
-
-            return View(room);
-        }
+        
     }
 }
