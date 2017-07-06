739fa9270e4ef83a271ea186cb5ee430f9b58329
yamachi4416 <yamachi4416@gmail.com>
2017-07-06 17:52:10 +0900

    Fix Gruntfile.js add "use strict"

--- src/ChatApp/Gruntfile.js
+++ src/ChatApp/Gruntfile.js
@@ -3,6 +3,7 @@ This file in the main entry point for defining grunt tasks and using grunt plugi
 Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
 */
 module.exports = function (grunt) {
+    "use strict";
     const paths = require('path');
     const fs = require('fs');
 
