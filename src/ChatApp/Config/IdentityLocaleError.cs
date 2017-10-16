7d81edb48cce1a39ad6c56f104236cd197944fe6
yamachi4416 <yamachi4416@gmail.com>
2017-10-16 23:34:24 +0900

    機能 #75 ローカライゼーション パスワード回り

--- src/ChatApp/Config/IdentityLocaleError.cs
+++ src/ChatApp/Config/IdentityLocaleError.cs
@@ -19,6 +19,27 @@ namespace ChatApp.IdentityLocaleError
         public override IdentityError DuplicateUserName(string userName)
             => GetError(nameof(DuplicateUserName), "User name '{0}' is already taken.", userName);
 
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+            => GetError(nameof(PasswordRequiresNonAlphanumeric), "Passwords must have at least one non alphanumeric character.");
+        
+        public override IdentityError PasswordRequiresDigit()
+            => GetError(nameof(PasswordRequiresDigit), "Passwords must have at least one digit ('0'-'9').");
+
+        public override IdentityError PasswordRequiresUpper()
+            => GetError(nameof(PasswordRequiresUpper), "Passwords must have at least one uppercase ('A'-'Z').");
+
+        public override IdentityError PasswordMismatch()
+            => GetError(nameof(PasswordMismatch), "Incorrect password.");
+
+        public override IdentityError PasswordRequiresLower()
+            => GetError(nameof(PasswordRequiresLower), "Passwords must have at least one lowercase ('a'-'z').");
+
+        public override IdentityError PasswordRequiresUniqueChars(int leastCount)
+            => GetError(nameof(PasswordRequiresUniqueChars), "Passwords must use at least {0} different characters.", leastCount);
+
+        public override IdentityError PasswordTooShort(int leastCount)
+            => GetError(nameof(PasswordTooShort), "Passwords must be at least {0} characters.", leastCount);
+
         private IdentityError GetError(string code, string message, params object[] arguments)
         {
             return new IdentityError
