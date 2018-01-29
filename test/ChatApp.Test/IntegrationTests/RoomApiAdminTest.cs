fd0e26b465881bd7dfb34fbe72c38577fe2ac06c
yamachi4416 <yamachi4416@gmail.com>
2018-01-29 22:13:04 +0900

    タスク #116 管理者がメンバーの削除ができることの確認のテストを追加

--- test/ChatApp.Test/IntegrationTests/RoomApiAdminTest.cs
+++ test/ChatApp.Test/IntegrationTests/RoomApiAdminTest.cs
@@ -62,5 +62,56 @@ namespace ChatApp.Test.IntegrationTests
             Assert.Equal(1, await fixture.DbContext.ChatRoomMembers
                 .Where(m => m.UserId == addUser.Id && m.ChatRoomId == chatRoom.Id).CountAsync());
         }
+
+        [Fact(DisplayName = "ルームの管理者はルームにメンバーを削除できること")]
+        public async void RoomApiAdmin_RemoveRoom_Success()
+        {
+            var user = await dataCreator.CreateUserAsync();
+            var chatRooms = dataCreator.GetChatRooms(user).Take(2).ToList();
+
+            var users = dataCreator.GetTestUsers().Skip(1).Take(2).ToList();
+            await fixture.DbContext.AddRangeAsync(users);
+            await fixture.DbContext.SaveChangesAsync();
+
+            var adminMember = dataCreator.GetChatRoomMember(chatRooms[0], user);
+            adminMember.IsAdmin = true;
+            var member1 = dataCreator.GetChatRoomMember(chatRooms[0], users[0]);
+            var member2 = dataCreator.GetChatRoomMember(chatRooms[1], users[1]);
+
+            await fixture.DbContext.AddRangeAsync(chatRooms);
+            await fixture.DbContext.AddRangeAsync(adminMember, member1, member2);
+            await fixture.DbContext.SaveChangesAsync();
+
+            var requestPath = sitePath[$"/{chatRooms[0].Id}/members/remove"];
+            var browser = await fixture.CreateWebBrowserWithLoginAsyc(user);
+            await browser.FollowRedirectAsync();
+
+            // ルームのメンバーを削除できること
+            var result = await browser.PostJsonDeserializeResultAsync<RoomMemberViewModel>(
+                requestPath, new RoomMemberViewModel { Id = member1.UserId });
+
+            Assert.Equal(member1.UserId, result.Id);
+            Assert.Empty(await fixture.DbContext.ChatRoomMembers
+                .Where(m => m.UserId == member1.UserId)
+                .ToListAsync());
+
+            // ほかのルームのメンバーは削除できないこと
+            result = await browser.PostJsonDeserializeResultAsync<RoomMemberViewModel>(
+                requestPath, new RoomMemberViewModel { Id = member2.UserId });
+
+            Assert.Null(result);
+            browser.Response.EnsureSuccessStatusCode();
+            Assert.NotEmpty(await fixture.DbContext.ChatRoomMembers
+                .Where(m => m.UserId == member2.UserId).ToListAsync());
+
+            // 管理者は削除できないこと
+            result = await browser.PostJsonDeserializeResultAsync<RoomMemberViewModel>(
+                requestPath, new RoomMemberViewModel { Id = adminMember.UserId });
+
+            Assert.Null(result);
+            browser.Response.EnsureSuccessStatusCode();
+            Assert.NotEmpty(await fixture.DbContext.ChatRoomMembers
+                .Where(m => m.UserId == adminMember.UserId).ToListAsync());
+        }
     }
 }
